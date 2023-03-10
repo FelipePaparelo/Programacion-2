a8c387b6a2d9bc103b8f362db176b4585c7b0ec2
agustinirrazabal1932 <83938507+agustinirrazabal1932@users.noreply.github.com>
2023-03-10 17:57:05 -0300

    prueba en casa

--- hola-mundo.c
+++ hola-mundo.c
@@ -4,9 +4,7 @@ int main(){
     int i;
     printf("Hola Mundo desde github desktop\n");
     printf("Buenas noches");
-
     printf("river campeon de  la libertadores");
-
     printf("bocaaaaaaaaaaaaaaaaaaaaaaaaaa campeon");
 
     return 0;
