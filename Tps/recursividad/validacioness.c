4c1249b4b8b22dc9f2d84054ed9426fa9f92b20c
Renzo Bisso <83300988+RenzoBisso@users.noreply.github.com>
2023-04-09 11:53:14 -0300

    carpeta principal

---
+++ Tps/recursividad/validacioness.c
@@ -0,0 +1,363 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <ctype.h>
+#include <string.h>
+#include <conio.h>
+#include <stdlib.h>
+
+char validar_numero_menu(char* m, int *f)
+{
+    int aux = 0;
+    int bandera = 1;
+    bool flag2 = false;
+    printf("\nIngresa una opcion del 1 al 10: ");
+    fgets(m, 100,stdin);
+    int largo = strlen(m) - 1;
+    while (bandera == 1)
+    {
+        if (strcmp(m, "\n") == 0){
+        printf("Has ingresado un salto de línea.\n");
+        aux = -1;
+        } 
+
+        for(int j = 0; j<largo;j++){
+            
+            if((isdigit(m[j]) != 0)){
+                    aux++;
+            }
+        }
+        
+        if (aux == largo){
+            bandera = 0;
+        }
+        else{
+            aux = 0;
+            printf("Ingresa un numero otra vez: ");
+            fgets(m, 100,stdin);
+            fflush(stdin);
+            largo = strlen(m) - 1;
+        }
+        if (m == 0){
+            *f = 0;
+        }
+    }
+}
+
+void validacion_mayus_minus(char palabra[]) {
+    printf("Ingrese un palindromo a comprobar: ");
+    fgets(palabra, 100, stdin);
+    for (int i = 0; i < strlen(palabra); i++) {
+            palabra[i] = tolower(palabra[i]);
+        if (palabra[strlen(palabra)-1] == '\n'){
+            palabra[strlen(palabra)-1] = '\0';
+        }
+    }
+}
+
+
+char validar_punto2(char* m)
+{
+    int aux = 0;
+    int bandera = 1;
+    bool flag2 = false;
+    printf("Ingresa un numero entero(m): ");
+    fgets(m, 100,stdin);
+    int largo = strlen(m) - 1;
+    while (bandera == 1)
+    {
+        if (strcmp(m, "\n") == 0){
+            printf("Has ingresado un salto de linea.\n");
+            aux = -1;
+        }
+
+        for(int j = 0; j<largo;j++){
+
+            if ((j==0)&&(m[j]=='-')){
+                aux++;
+            }
+
+            if((isdigit(m[j]) != 0)){
+                aux++;
+            }
+        }
+
+        if (aux == largo){
+            printf("exitoso, es un numero entero(n)\n");
+            bandera = 0;
+        }
+        else{
+            aux = 0;
+            printf(" el dato que Ingresa no es un numero entero, intente otra vez: ");
+            fgets(m, 100,stdin);
+            fflush(stdin);
+            largo = strlen(m) - 1;
+        }
+    }
+
+}
+
+int validar_punto3(char* input) {
+    int num;
+    bool es_valido = false;
+    while (!es_valido) {
+        printf("Ingresa un numero entero a comprobar de la sucesion de Fibonacci entre 0 y 40: ");
+        fgets(input, 100, stdin);
+        int largo = strlen(input) - 1;
+        if (largo == 0) {
+            printf("No ingresaste ningun valor.\n");
+            continue;
+        }
+        bool es_entero = true;
+        for (int i = 0; i < largo; i++) {
+            if (!isdigit(input[i])) {
+                es_entero = false;
+                printf("No ingresaste un valor valido.\n");
+                break;
+            }
+        }
+        if (es_entero) {
+            num = atoi(input);
+            if (num >= 0 && num <= 40) {
+                es_valido = true;
+        } else {
+            printf("No ingresaste un valor valido.\n");
+            }
+        } 
+    } 
+    return num;
+}
+
+
+
+int validar_punto4(char* input) {
+    int num;
+    bool es_valido = false;
+    while (!es_valido) {
+        printf("Ingresa un numero entero : ");
+        fgets(input, 100, stdin);
+        int largo = strlen(input) - 1;
+        if (largo == 0) {
+            printf("No ingresaste ningun valor.\n");
+            continue;
+        }
+        bool es_entero = true;
+        for (int i = 0; i < largo; i++) {
+            if (!isdigit(input[i])) {
+                es_entero = false;
+                break;
+            }
+        }
+        if (es_entero) {
+            num = atoi(input);
+            es_valido = true;
+        } else {
+            printf("No ingresaste un numero entero valido.\n");
+        }
+    }
+    return num;
+}
+
+char validar_punto5(char* m)
+{
+    int aux = 0;
+    int bandera = 1;
+    printf("Ingresar numero: ");
+    fgets(m, 100,stdin);
+    int largo = strlen(m) - 1;
+    while (bandera == 1)
+    {
+        if (strcmp(m, "\n") == 0){
+            printf("Has ingresado un salto de linea.\n");
+            aux = -1;
+        }
+
+        for(int j = 0; j<largo;j++){
+
+            if ((j==0)&&(m[j]=='-')){
+                aux++;
+            }
+
+            if((isdigit(m[j]) != 0)){
+                aux++;
+            }
+        }
+
+        if (aux == largo){
+            printf("exitoso, es un numero entero\n");
+            bandera = 0;
+        }
+
+        else{
+            aux = 0;
+            printf(" el dato que Ingresa no es un numero entero, intente otra vez: ");
+            fgets(m, 100,stdin);
+            fflush(stdin);
+            largo = strlen(m) - 1;
+        }
+    }
+
+}
+
+char validar_punto6(char* m)
+{
+    int aux = 0;
+    int bandera = 1;
+    printf("Ingresa un nivel: ");
+    fgets(m, 100,stdin);
+    int largo = strlen(m) - 1;
+    while (bandera == 1)
+    {
+        if (strcmp(m, "\n") == 0){
+            printf("Has ingresado un salto de linea.\n");
+            aux = -1;
+        }
+
+        for(int j = 0; j<largo;j++){
+
+            if ((j==0)&&(m[j]=='-')){
+                aux++;
+            }
+
+            if((isdigit(m[j]) != 0)){
+                aux++;
+            }
+        }
+
+        if (aux == largo){
+            bandera = 0;
+        }
+        else{
+            aux = 0;
+            printf("el dato que Ingresa no es valido, intente otra vez: ");
+            fgets(m, 100,stdin);
+            fflush(stdin);
+            largo = strlen(m) - 1;
+        }
+    }
+
+}
+
+char validar_punto7(char* m)
+{
+    int aux = 0;
+    int bandera = 1;
+    char ayuda;
+    printf("Ingresa un l o h para la onda digital: ");
+    fgets(m, 100,stdin);
+    int largo = strlen(m) - 1;
+    while (bandera == 1)
+    {
+        if (strcmp(m, "\n") == 0){
+            printf("Has ingresado un salto de linea.\n");
+            aux = -1;
+        }
+
+        for(int j = 0; j<largo;j++){
+           if(m[j] == 'H' || m[j] == 'h'){
+            aux++;
+           }
+           if(m[j] == 'L' || m[j] == 'l'){
+            aux++;
+           }
+        }
+
+        if (aux == largo){
+            bandera = 0;
+        }
+        else{
+            aux = 0;
+            printf("el dato que Ingresa no es valido, intente otra vez: ");
+            fgets(m, 100,stdin);
+            fflush(stdin);
+            largo = strlen(m) - 1;
+        }
+    }
+
+}
+
+
+
+char validar_punto9(char* m)
+{
+    int aux = 0;
+    int bandera = 1;
+    printf("Ingresa un numero entero: ");
+    fgets(m, 100,stdin);
+    int largo = strlen(m) - 1;
+    while (bandera == 1)
+    {
+        if (strcmp(m, "\n") == 0){
+            printf("Has ingresado un salto de linea.\n");
+            aux = -1;
+        }
+
+        for(int j = 0; j<largo;j++){
+
+            if ((j==0)&&(m[j]=='-')){
+                aux++;
+            }
+
+            if((isdigit(m[j]) != 0)){
+                aux++;
+            }
+        }
+
+        if (aux == largo){
+            printf("exitoso, es un numero entero\n");
+            bandera = 0;
+        }
+        else{
+            aux = 0;
+            printf(" el dato que Ingresa no es un numero entero, intente otra vez: ");
+            fgets(m, 100,stdin);
+            fflush(stdin);
+            largo = strlen(m) - 1;
+        }
+    }
+
+}
+
+
+
+char validar_punto10(char* m)
+{
+    int aux = 0;
+    int bandera = 1;
+    printf("Ingresa un numero entero: ");
+    fgets(m, 100,stdin);
+    int largo = strlen(m) - 1;
+    while (bandera == 1)
+    {
+        if (strcmp(m, "\n") == 0){
+            printf("Has ingresado un salto de linea.\n");
+            aux = -1;
+        }
+
+        for(int j = 0; j<largo;j++){
+
+            if ((j==0)&&(m[j]=='-')){
+                aux++;
+            }
+
+            if((isdigit(m[j]) != 0)){
+                aux++;
+            }
+        }
+
+        if (aux == largo){
+            printf("exitoso, es un numero entero\n");
+            bandera = 0;
+        }
+        else{
+            aux = 0;
+            printf(" el dato que Ingresa no es un numero entero, intente otra vez: ");
+            fgets(m, 100,stdin);
+            fflush(stdin);
+            largo = strlen(m) - 1;
+        }
+    }
+
+}
+
+
+
