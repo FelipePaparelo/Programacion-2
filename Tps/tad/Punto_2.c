59a06c15dc4a3ca3566573f6eff648df759d4af9
Renzo Bisso <renzo.bissoo@gmail.com>
2023-04-18 23:26:27 -0300

    Punto 4 hecho

--- Tps/tad/Punto_2.c
+++ Tps/tad/Punto_2.c
@@ -9,6 +9,43 @@
 // #include "list_cursor.c"
 // #include "listas_de_areglos.c"
 #include "T_Element.c"
+char validar_numeros_positivos(char *m)
+{
+    int aux = 0;
+    int bandera = 1;
+    fgets(m, 100, stdin);
+    int largo = strlen(m) - 1;
+    while (bandera == 1)
+    {
+        if (strcmp(m, "\n") == 0)
+        {
+            printf("Has ingresado un salto de linea.\n");
+            aux = -1;
+        }
+
+        for (int j = 0; j < largo; j++)
+        {
+            if ((isdigit(m[j]) != 0))
+            {
+                aux++;
+            }
+        }
+
+        if (aux == largo)
+        {
+            bandera = 0;
+        }
+        else
+        {
+            aux = 0;
+            printf("El dato que Ingresa no es un numero positivo, intente otra vez: ");
+            fgets(m, 100, stdin);
+            fflush(stdin);
+            largo = strlen(m) - 1;
+        }
+    }
+    return bandera;
+}
 
 char validar_numeros(char *m)
 {
@@ -54,43 +91,19 @@ char validar_numeros(char *m)
     return bandera;
 }
 
-char validar_numeros_positivos(char *m)
+void cargar_datos_teclado(int dato_i, Lista list_1)
 {
-    int aux = 0;
-    int bandera = 1;
-    fgets(m, 100, stdin);
-    int largo = strlen(m) - 1;
-    while (bandera == 1)
+    TipoElemento x;
+    char cad[100];
+    int i, numero;
+    for (i = 0; i < dato_i; ++i)
     {
-        if (strcmp(m, "\n") == 0)
-        {
-            printf("Has ingresado un salto de linea.\n");
-            aux = -1;
-        }
-
-        for (int j = 0; j < largo; j++)
-        {
-
-            if ((isdigit(m[j]) != 0))
-            {
-                aux++;
-            }
-        }
-
-        if (aux == largo)
-        {
-            bandera = 0;
-        }
-        else
-        {
-            aux = 0;
-            printf("El dato que Ingresa no es un numero positivo, intente otra vez: ");
-            fgets(m, 100, stdin);
-            fflush(stdin);
-            largo = strlen(m) - 1;
-        }
+        printf("ingrese el valor para la clave del %i tipoelemento:\n", 1 + i);
+        validar_numeros(cad);
+        numero = atoi(cad);
+        x = te_crear(numero);
+        l_agregar(list_1, x);
     }
-    return bandera;
 }
 
 void cargar_datos_alazar(int dato_i, Lista list_1)
@@ -155,21 +168,6 @@ void resolver_punto2_c(Lista list_1, int largo, float *result)
         x = l_recuperar(list_1, largo);
         *result += x->clave;
         resolver_punto2_c(list_1, largo - 1, result);
-    }
-}
-
-void cargar_datos_teclado(int dato_i, Lista list_1)
-{
-    TipoElemento x;
-    char cad[100];
-    int i, numero;
-    for (i = 0; i < dato_i; ++i)
-    {
-        printf("ingrese el valor para la clave del %i tipoelemento:\n", 1 + i);
-        validar_numeros(cad);
-        numero = atoi(cad);
-        x = te_crear(numero);
-        l_agregar(list_1, x);
     }
 }
 
--- Tps/tad/Punto_4.c
+++ Tps/tad/Punto_4.c
@@ -5,53 +5,166 @@
 #include <string.h>
 #include "listas.h"
 #include "tipo_elemento.h"
-#include "list_point.c"
-// #include "list_cursor.c"
+// #include "list_point.c"
+#include "list_cursor.c"
 // #include "listas_de_areglos.c"
 #include "T_Element.c"
 
+char validar_numeros(char *m)
+{
+    int aux = 0;
+    int bandera = 1;
+    fgets(m, 100, stdin);
+    int largo = strlen(m) - 1;
+    while (bandera == 1)
+    {
+        if (strcmp(m, "\n") == 0)
+        {
+            printf("Has ingresado un salto de linea.\n");
+            aux = -1;
+        }
+
+        for (int j = 0; j < largo; j++)
+        {
+
+            if ((j == 0) && (m[j] == '-'))
+            {
+                aux++;
+            }
+
+            if ((isdigit(m[j]) != 0))
+            {
+                aux++;
+            }
+        }
+
+        if (aux == largo)
+        {
+            bandera = 0;
+        }
+        else
+        {
+            aux = 0;
+            printf("El dato que Ingresa no es un numero entero, intente otra vez: ");
+            fgets(m, 100, stdin);
+            fflush(stdin);
+            largo = strlen(m) - 1;
+        }
+    }
+}
+
+char validar_numeros_positivos(char *m)
+{
+    int aux = 0;
+    int bandera = 1;
+    fgets(m, 100, stdin);
+    int largo = strlen(m) - 1;
+    while (bandera == 1)
+    {
+        if (strcmp(m, "\n") == 0)
+        {
+            printf("Has ingresado un salto de linea.\n");
+            aux = -1;
+        }
+
+        for (int j = 0; j < largo; j++)
+        {
+
+            if ((isdigit(m[j]) != 0))
+            {
+                aux++;
+            }
+        }
+
+        if (aux == largo)
+        {
+            bandera = 0;
+        }
+        else
+        {
+            aux = 0;
+            printf("El dato que Ingresa no es un numero positivo, intente otra vez: ");
+            fgets(m, 100, stdin);
+            fflush(stdin);
+            largo = strlen(m) - 1;
+        }
+    }
+}
+
+void cargar_datos_L1_teclado(int dato_i, Lista L1)
+{
+    TipoElemento x;
+    char cad[100];
+    int i, numero;
+    for (i = 0; i < dato_i; i++)
+    {
+        printf("ingrese el valor para la clave del %i tipoelemento:\n", 1 + i);
+        validar_numeros(cad);
+        numero = atoi(cad);
+        x = te_crear(numero);
+        l_agregar(L1, x);
+    }
+}
+
+void cargar_datos_L2_teclado(int dato_i, Lista L2)
+{
+    TipoElemento x;
+    char cad[100];
+    int i, numero;
+    for (i = 0; i < dato_i; i++)
+    {
+        printf("ingrese el valor para la clave del %i tipoelemento:\n", 1 + i);
+        validar_numeros(cad);
+        numero = atoi(cad);
+        x = te_crear(numero);
+        l_agregar(L2, x);
+    }
+}
+
 int compararListas(Lista L1, Lista L2)
 {
-    int i = 0;
+    int i = 1;
     int cant_claves_L1 = 0, cant_claves_L2 = 0;
-    TipoElemento elem_L1 = L1->inicio;
-    TipoElemento elem_L2 = L2->inicio;
+    TipoElemento elem_L1 = l_recuperar(L1, i);
+    TipoElemento elem_L2 = l_recuperar(L2, i);
 
     // recorremos ambas listas hasta que una de ellas se termine
-    while (elem_L1 != NULL && elem_L2 != NULL)
+    while (elem_L1 != NULL && elem_L2 != NULL && i <= l_longitud(L1) && i <= l_longitud(L2))
     {
         if (elem_L1->clave > elem_L2->clave)
         {
             cant_claves_L1++;
-            elem_L1 = L1->inicio->siguiente;
+            i++;
+            elem_L1 = l_recuperar(L1, i);
         }
         else if (elem_L1->clave < elem_L2->clave)
         {
             cant_claves_L2++;
-            elem_L2 = L2->inicio->siguiente;
+            i++;
+            elem_L2 = l_recuperar(L2, i);
         }
         else
-        { // si las claves son iguales avanzamos en ambas listas
-            elem_L1 = L1->inicio->siguiente;
-            elem_L2 = L2->inicio->siguiente;
+        {
+            i++;
+            elem_L1 = l_recuperar(L1, i);
+            elem_L2 = l_recuperar(L2, i);
         }
-        i++;
     }
 
     // si quedaron elementos en L1, es porque L1 es mayor que L2
-    while (elem_L1 != NULL)
+    while (elem_L1 != NULL && i <= l_longitud(L1))
     {
         cant_claves_L1++;
-        elem_L1 = L1->inicio->siguiente;
         i++;
+        elem_L1 = l_recuperar(L1, i);
     }
 
     // si quedaron elementos en L2, es porque L2 es mayor que L1
-    while (elem_L2 != NULL)
+    while (elem_L2 != NULL && i <= l_longitud(L2))
     {
         cant_claves_L2++;
-        elem_L2 = L2->inicio->siguiente;
         i++;
+        elem_L2 = l_recuperar(L2, i);
     }
 
     if (cant_claves_L1 > cant_claves_L2)
@@ -65,5 +178,37 @@ int compararListas(Lista L1, Lista L2)
     else
     {
         return 0; // L1 = L2
+    }
+}
+
+int main()
+{
+    char cad_l1[100];
+    int num_l1;
+    Lista l1, l2;
+    printf("\nBIENENIDO AL PUNTO 3 DEL TP DE LISTAS\n");
+    printf("CUANTOS ELEMENTOS VA INGRESAR EN LA LISTA 1:\n");
+    validar_numeros_positivos(cad_l1);
+    num_l1 = atoi(cad_l1);
+    printf("COMIENZO DE CARGA DE LA LISTA 1:\n");
+    l1 = l_crear();
+    cargar_datos_L1_teclado(num_l1, l1);
+    printf("SE CARGO CORRECTAMENTE LA LISTA 1:\n");
+    printf("COMIENZO DE CARGA DE LA LISTA 2:\n");
+    l2 = l_crear();
+    cargar_datos_L2_teclado(num_l1, l2);
+    printf("SE CARGO CORRECTAMENTE LA LISTA 2:\n");
+    int resultado = compararListas(l1, l2);
+    if (resultado == 1)
+    {
+        printf("La lista 1 es mayor que la lista 2");
+    }
+    else if (resultado == -1)
+    {
+        printf("La lista 2 es mayor que la lista 1");
+    }
+    else
+    {
+        printf("Las listas 1 y 2 son iguales");
     }
 }
