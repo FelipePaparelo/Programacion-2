76acf458e91ee0a3a006ccc0324b4e86110bcaa6
Renzo Bisso <renzo.bissoo@gmail.com>
2023-05-12 19:59:05 -0300

    Punto2: realizado

--- Tps/colas/punto2.c
+++ Tps/colas/punto2.c
@@ -19,8 +19,8 @@ f. Invertir del contenido de una cola sin destruir la cola original.
 // #include "colas_arreglos_circular.c"
 #include "colas_punteros.c"
 #include <stdbool.h>
-// #include "../pilas/pilas.h"
-// #include "../pilas/pilas_arreglos.c"
+#include "../pilas/pilas.h"
+#include "../pilas/pilas_arreglos.c"
 
 void validar_numeros(char *m)
 {
@@ -73,7 +73,22 @@ void validar_numeros(char *m)
         }
     }
 }
+void vaciar_cola(Cola c)
+{
+    int longitud = c_longitud(c);
+    for (int i = 0; i < longitud; i++)
+    {
+        TipoElemento elem = c_desencolar(c);
+        free(elem);
+    }
+}
 
+int cargar_n()
+{
+    char cad[100];
+    validar_numeros(cad);
+    return atoi(cad);
+}
 void validar_numeros_positivos(char *m)
 {
     int aux = 0;
@@ -143,7 +158,7 @@ void cargar_datos_alazar(int dato_i, Cola c_azar)
         r = rand(); // Se genera un número aleatorio entre 0 y RAND_MAX
         r = r % 11; // Se obtiene un número entre 0 y 100 utilizando el operador módulo
         x = te_crear(r);
-        p_apilar(c_azar, x);
+        c_encolar(c_azar, x);
     }
 }
 int cargar_largo()
@@ -154,15 +169,15 @@ int cargar_largo()
     return dato_i;
 }
 
-bool esta(Cola c1, int n, int dato_i)
+bool esta(Cola c1, int dato_i)
 {
     cargar_datos_teclado(dato_i, c1);
     if (c_es_vacia(c1))
     {
         return false;
     }
-    printf("Ingrese el numero a buscar:\n");
-
+    printf("Ingrese el numero a buscar:");
+    int n = cargar_n();
     Cola c1_aux = c_crear();
     int longitud = c_longitud(c1);
     int contador = 0;
@@ -177,44 +192,64 @@ bool esta(Cola c1, int n, int dato_i)
     }
     if (contador > 0)
     {
+        printf("Esta\n");
         return true;
     }
     else
     {
+        printf("No esta\n");
         return false;
     }
 }
 
-void agregado(Cola c1, int pos, TipoElemento j)
+void agregado(Cola c1, int dato_i)
 {
+    cargar_datos_teclado(dato_i, c1);
     if (c_es_vacia(c1))
     {
-
         return;
+    }
+    printf("Ingrese el numero a agregar:\n");
+    int n = cargar_n();
+    TipoElemento j = te_crear(n);
+    printf("Ingrese la posicion:\n");
+    int pos = cargar_n();
+    if (pos > dato_i)
+    {
+        printf("Valor invalido\n");
+        vaciar_cola(c1);
+        agregado(c1, dato_i);
     }
     Cola c1_aux = c_crear();
     TipoElemento aux = te_crear(0);
     int longitud = c_longitud(c1);
-    for (int k = 0; k < longitud; k++)
+    for (int k = 1; k <= longitud + 1; k++)
     {
         if (k == pos)
         {
-            TipoElemento x = c_desencolar(c1);
             c_encolar(c1_aux, j);
-            aux = x;
-            c_encolar(c1_aux, aux);
         }
-        TipoElemento x = c_desencolar(c1);
-        c_encolar(c1_aux, x);
+        if (!c_es_vacia(c1))
+        {
+            TipoElemento x = c_desencolar(c1);
+            c_encolar(c1_aux, x);
+        }
     }
     c_mostrar(c1_aux);
 }
 
-void borrar(Cola c1, int n)
+void borrar(Cola c1, int dato_i)
 {
+    cargar_datos_teclado(dato_i, c1);
+    if (c_es_vacia(c1))
+    {
+        return;
+    }
     Cola cola_borrados = c_crear();
     int largo = c_longitud(c1);
-    TipoElemento x;
+    printf("Ingrese el numero a borrar:\n");
+    int n = cargar_n();
+    TipoElemento x = te_crear(n);
     for (int k = 0; k < largo; k++)
     {
         x = c_desencolar(c1);
@@ -223,42 +258,52 @@ void borrar(Cola c1, int n)
             c_encolar(cola_borrados, x);
         }
     }
+    c_mostrar(cola_borrados);
     return cola_borrados;
 }
 
-void repetido(Cola c1, int n)
+// void repetido(Cola c1, int dato_i)
+// {
+//     cargar_datos_teclado(dato_i, c1);
+//     if (c_es_vacia(c1))
+//     {
+//         return;
+//     }
+//     printf("Ingrese el numero a agregar:\n");
+//     int n = cargar_n();
+//     TipoElemento j = te_crear(n);
+//     Cola c_aux = c_crear();
+//     bool esta = false;
+//     int longitud = c_longitud(c1);
+//     for (int k = 0; k < longitud; k++)
+//     {
+//         TipoElemento x = c_desencolar(c1);
+//         if (x->clave != n)
+//         {
+//             c_encolar(c_aux, x);
+//         }
+//         else
+//         {
+//             esta = true;
+//         }
+//     }
+//     if (esta == false)
+//     {
+//         printf("No esta");
+//     }
+//     else
+//     {
+//         c_mostrar(c_aux);
+//     }
+// }
+
+void cantidad(Cola c1, int dato_i)
 {
+    cargar_datos_teclado(dato_i, c1);
     if (c_es_vacia(c1))
     {
         return;
     }
-    Cola c_aux = c_crear();
-    bool esta = false;
-    int longitud = c_longitud(c1);
-    for (int k = 0; k < longitud; k++)
-    {
-        TipoElemento x = c_desencolar(c1);
-        if (x->clave != n)
-        {
-            c_encolar(c_aux, x);
-        }
-        else
-        {
-            esta = true;
-        }
-    }
-    if (esta == false)
-    {
-        printf("No esta");
-    }
-    else
-    {
-        c_mostrar(c_aux);
-    }
-}
-
-void cantidad(Cola c1)
-{
     if (c_es_vacia(c1))
     {
         return;
@@ -267,8 +312,9 @@ void cantidad(Cola c1)
     printf("Hay %i elementos en la cola\n", cantidad_elemt);
 }
 
-void copia(Cola c1)
+void copia(Cola c1, int dato_i)
 {
+    cargar_datos_teclado(dato_i, c1);
     if (c_es_vacia(c1))
     {
         return;
@@ -276,52 +322,62 @@ void copia(Cola c1)
     Cola c1_copia = c_crear();
     Cola c1_original = c_crear();
     int longitud = c_longitud(c1);
-    for (int k = 0; k < longitud; k++)
+    TipoElemento x;
+
+    for (int k = 0; k < longitud / 2; k++)
     {
-        TipoElemento x = c_desencolar(c1);
+        x = c_desencolar(c1);
         c_encolar(c1_original, x);
         c_encolar(c1_copia, x);
     }
+    printf("Cola original:\n");
     c_mostrar(c1_original);
+    printf("Cola copiada:\n");
     c_mostrar(c1_copia);
 }
 
-// void invertir(Cola c1)
-// {
-//     if (c_es_vacia(c1))
-//     {
-//         return;
-//     }
-//     int longitud = c_longitud(c1);
+void invertir(Cola c1, int dato_i)
+{
+    cargar_datos_teclado(dato_i, c1);
+    if (c_es_vacia(c1))
+    {
+        return;
+    }
+    int longitud = c_longitud(c1);
 
-//     Cola c1_copia = c_crear();
-//     Cola c1_rever = c_crear();
-//     Pila pila_aux = p_crear();
-//     for (int k = 0; k < longitud; k++)
-//     {
-//         TipoElemento x = c_desencolar(c1);
-//         c_encolar(c1_copia, x);
-//         p_apilar(pila_aux, x);
-//     }
-//     for (int j = 0; j < longitud; j++)
-//     {
-//         TipoElemento y = p_desapilar(pila_aux);
-//         c_encolar(c1_rever, y);
-//     }
+    Pila pila_aux = p_crear();
+    for (int k = 0; k < longitud; k++)
+    {
+        TipoElemento x = c_desencolar(c1);
+        p_apilar(pila_aux, x);
+    }
+    for (int j = 0; j < longitud; j++)
+    {
+        TipoElemento y = p_desapilar(pila_aux);
+        c_encolar(c1, y);
+    }
 
-//     c_mostrar(c1_copia);
-//     c_mostrar(c1_rever);
-// }
+    c_mostrar(c1);
+}
 
 int main()
 {
     Cola c1 = c_crear();
-    int n, dato_i;
+    int dato_i, n;
     TipoElemento q;
     printf("Ingrese el largo de la cola:\n");
-    largo = cargar_largo();
-    printf("###########PUNTO 2-a###########");
-    esta(c1, n);
-
+    int largo = cargar_largo();
+    printf("###########PUNTO 2-a###########\n");
+    esta(c1, largo);
+    printf("###########PUNTO 2-b###########\n");
+    agregado(c1, largo);
+    printf("###########PUNTO 2-c###########\n");
+    borrar(c1, largo);
+    printf("###########PUNTO 2-d###########\n");
+    cantidad(c1, largo);
+    printf("###########PUNTO 2-e###########\n");
+    copia(c1, largo);
+    printf("###########PUNTO 2-f###########\n");
+    invertir(c1, largo);
     return 0;
 }
