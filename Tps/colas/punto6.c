0542504de5f5b9bc57797cfa602bc55b56c296e4
Felipe Paparelo <felipe_paparelo01@hotmail.com>
2023-05-15 10:33:02 -0300

    cambios en punto 6

--- Tps/colas/punto6.c
+++ Tps/colas/punto6.c
@@ -17,157 +17,6 @@
 const int TAM_MAXIMO = 100;
 
 
-
-
-// 6. Dada una pila y una cola generada con valores al azar retornar en una lista todos los
-// valores comunes a ambas y en qué posición ordinal se encontró cada uno en su
-// estructura. No se deben destruir las estructuras originales. No se deben perderse las
-// estructuras originales. Determinar la complejidad algorítmica de la solución empleada.
-// Ejemplo: si “P” = (2,5,8,19,3,4) y “C” = (4, 18, 12, 5, 6) la lista tendría L = (5:2:4, 4:6:5). 
-
-void validar_numeros(char *m)
-{
-    int aux = 0;
-    int bandera = 1;
-    int bandera_2;
-    fgets(m, 100, stdin);
-    int largo = strlen(m) - 1;
-    while (bandera == 1)
-    {
-        if (strcmp(m, "\n") == 0)
-        {
-            printf("Has ingresado un salto de linea.\n");
-            aux = -1;
-        }
-
-        for (int j = 0; j < largo; j++)
-        {
-
-            if ((j == 0) && (m[j] == '-'))
-            {
-                aux++;
-            }
-
-            if ((isdigit(m[j]) != 0))
-            {
-                aux++;
-            }
-        }
-
-        bandera_2 = 0;
-        if (aux == largo && aux <= 9)
-        {
-            if (bandera_2 == 1)
-            {
-                bandera = 1;
-            }
-            else if (bandera_2 == 0)
-            {
-                bandera = 0;
-            }
-        }
-        else
-        {
-            aux = 0;
-            printf("intente otra vez: ");
-            fgets(m, 100, stdin);
-            fflush(stdin);
-            largo = strlen(m) - 1;
-        }
-    }
-}
-
-
-
-void p_intercambiar(Pila pAux, Pila p)
-{
-    TipoElemento te;
-
-    while (p_es_vacia(pAux) != true)
-    {
-        te = p_desapilar(pAux);
-        p_apilar(p, te);
-    }
-}
-
-
-void validar_numeros_no_rep_pila(char *m, Pila pila)
-{
-    int aux = 0;
-    Pila p_aux = p_crear();
-    int bandera = 1, p, num_de_m, bandera_2;
-    TipoElemento x;
-    fgets(m, 100, stdin);
-    int largo = strlen(m) - 1;
-    while (bandera == 1)
-    {
-        if (strcmp(m, "\n") == 0)
-        {
-            printf("Has ingresado un salto de linea.\n");
-            aux = -1;
-        }
-
-        for (int j = 0; j < largo; j++)
-        {
-
-            if ((j == 0) && (m[j] == '-'))
-            {
-                aux++;
-            }
-
-            if ((isdigit(m[j]) != 0))
-            {
-                aux++;
-            }
-        }
-
-        bandera_2 = 0;
-        num_de_m = atoi(m);
-        if (aux == largo && aux <= 9)
-        {
-
-            if (num_de_m > 100000000 || num_de_m < -10000000)
-            {
-                bandera_2 = 1;
-                aux = -1;
-                printf("ingrese un numero mayor a -10.000.000 o menor a 100.000.000\n");
-            }
-            else{
-                bandera_2 = 0;
-            }
-
-            while(!p_es_vacia(pila)){
-                x = p_desapilar(pila);
-                if (x->clave == num_de_m && bandera_2 == 0)
-                {
-                    bandera_2 = 1;
-                    aux = -1;
-                    printf("el numero ya existe en la pila, Ingrese un elemento distinto a los ingresados anteriormente.\n");
-                }
-                p_apilar(p_aux, x);
-            }
-            p_intercambiar(p_aux, pila);
-
-            
-
-            if (bandera_2 == 1){
-                bandera = 1;
-            }
-            else if (bandera_2 == 0){
-                bandera = 0;
-            }
-        }
-        else
-        {
-            aux = 0;
-            printf("intente otra vez: ");
-            fgets(m, 100, stdin);
-            fflush(stdin);
-            largo = strlen(m) - 1;
-        }
-    }
-}
-
 void strtrim(char *cadena) { // C.A.: O(n)
     char *comienzoDeCadena = cadena;
     char *finalDeCadena = cadena + strlen(cadena) - 1;
@@ -182,56 +31,6 @@ void strtrim(char *cadena) { // C.A.: O(n)
 }
 
 
-void cargar_datos_teclado_lista(int dato_i, Lista list_1)
-{
-    TipoElemento x;
-    char cad[100];
-    int i, numero;
-    for (i = 0; i < dato_i; ++i)
-    {
-        printf("ingrese el valor para la clave del %i tipoelemento:\n", 1 + i);
-        validar_numeros(cad);
-        numero = atoi(cad);
-        x = te_crear(numero);
-        l_agregar(list_1, x);
-    }
-}
-
-void cargar_datos_teclado_pila(int dato_i, Pila p1)
-{
-    TipoElemento x;
-    char cad[100];
-    int i, numero;
-    for (i = 0; i < dato_i; ++i)
-    {
-        printf("ingrese el valor para la clave del %i tipoelemento de la pila: ", 1 + i);
-        validar_numeros_no_rep_pila(cad, p1);
-        numero = atoi(cad);
-        x = te_crear(numero);
-        p_apilar(p1, x);
-    }
-    printf("Finalizo la Carga\n");
-    printf("\n\n");
-}
-
-void cargar_cola(Cola c,int cant){
-    int elemento = 0;
-    int iterador = 1;
-    char cadena[100];
-    TipoElemento x;
-    while (cant>0){
-        printf("ingrese el valor para la clave del %i tipoelemento de la cola: ", iterador);
-        validar_numeros(cadena);
-        elemento = atoi(cadena);
-        x = te_crear(elemento);
-        c_encolar(c,x);
-        iterador++;
-        elemento++;
-        cant--;
-    }
-    printf("Finalizo la Carga\n");
-    printf("\n\n");
-}
 
 bool estaEnLista(Lista L, int dato) { // C.A.: O(n)
     Iterador iter = iterador(L);
@@ -348,7 +147,8 @@ Pila copiar_pila(Pila pilaOriginal) { // C.A.: O(n)
 // valores comunes a ambas y en qué posición ordinal se encontró cada uno en su
 // estructura. No se deben destruir las estructuras originales. No se deben perderse las
 // estructuras originales. Determinar la complejidad algorítmica de la solución empleada.
-// Ejemplo: si “P” = (2,5,8,19,3,4) y “C” = (4, 18, 12, 5, 6) la lista tendría L = (5:2:4, 4:6:5). 
+// Ejemplo: si “P” = (2,5,8,19,3,4) y “C” = (4, 18, 12, 5, 6) la lista tendría L = (5:2:4, 4:6:5).
+ 
 void mostrar_posiciones_ordinales(Lista lista) { // C.A.: O(n)
 
     if (l_es_vacia(lista)) {
@@ -362,63 +162,43 @@ void mostrar_posiciones_ordinales(Lista lista) { // C.A.: O(n)
     }
 }
 
-    //     for ( int i = 0 ; i < tamanio_cola ; i++){
-    //         if(x->clave == vector_cola[i]){
-    //             char str[10];
-    //             if (pos > 0){
-    //                 strcat(str, ", ");
-    //             }
-    //             sprintf(str, "%i", i+1);
-    //             strcat(posiciones, str);
-    //             pos++;
-    //             // printf("dato que entra a valor: %s\n", posiciones);
-    //             x->valor = posiciones;
-    //             // printf("valor ciclo = %s\n", x->valor);
-    //             l_agregar(l1, x);
-    //         }
-    //     }
-    //     printf("valor  = %s\n", x->valor);
-    //     posiciones[0] = '\0';
-    //     printf("valor  = %s\n", x->valor);
-    // }
-    // l_mostrarListaConValor(l1);
-// }
 
-// void punto_6(Pila p1, Cola c1, Lista l1){  
-//     Cola c_aux = c_crear();
-//     int elemento_pila, elemento_cola;
-//     int vector_cola[100];
-//     TipoElemento x;
-//     TipoElemento y;
-//     TipoElemento z;
-//     int pos = 0;
-//     char posiciones[200] = "";
-//     int tamanio_pila = p_longitud(p1);
-//     int tamanio_cola = c_longitud(c1);
-//     for(int i = 0 ; i < tamanio_cola ; i++){
-//         x = c_desencolar(c1);
-//         vector_cola[i] = x->clave;
-//         c_encolar(c_aux, x);
-//     }
-//     while(!p_es_vacia(p1)){
-//         x = p_desapilar(p1);
+Lista repetidos_con_ordinal(Pila pila, Cola cola) { // C.A.: O(m * n), donde "m" es el tamaño de la pila y "n" es el tamaño de la cola
+    Lista lista = l_crear();
+    Cola copiaCola = copiar_cola(cola);
+    Pila copiaPila = copiar_pila(pila);
+    int contadorCola = 0;
+    int contadorPila = 0;
+    while (!p_es_vacia(copiaPila)) {
+
+        TipoElemento elementoActualPila = p_desapilar(copiaPila);
+        contadorPila++;
+
+        while (!c_es_vacia(copiaCola)) {
+
+            TipoElemento elementoActualCola = c_desencolar(copiaCola);
+            contadorCola++;
 
             if (elementoActualCola->clave == elementoActualPila->clave) {
+
                 char posP[10];
                 itoa(contadorPila, posP, 10);
                 char posC[10];
                 itoa(contadorCola, posC, 10);
+
                 char resultado[20] = "";
                 strcat(resultado, posP);
                 strcat(resultado, ":");
                 strcat(resultado, posC);
                 char* valorAux = (char*) malloc(sizeof(char) * 10);
                 strcpy(valorAux, resultado);
+
                 TipoElemento elementoAgregar = te_crear(elementoActualPila->clave);
                 elementoAgregar->valor = valorAux;
                 l_agregar(lista, elementoAgregar);
             }
         }
+
         contadorCola = 0;
         copiaCola = copiar_cola(cola);
     }
@@ -429,19 +209,17 @@ void mostrar_posiciones_ordinales(Lista lista) { // C.A.: O(n)
 
 int main() {
     
-    Pila pila = p_crear();
-    Cola cola = c_crear();
+    Pila pila = llenar_pila_al_azar();
+    Cola cola = llenar_cola_al_azar();
     int cantidad_elementos = 1;
     cargar_cola(cola, cantidad_elementos);
     cargar_datos_teclado_pila(cantidad_elementos, pila);
-    printf("------------------\n");
     p_mostrar(pila);
     c_mostrar(cola);
     printf("[REPETIDOS CON ORDINAL]\n");
     Lista repetidos = repetidos_con_ordinal(pila, cola);
     printf("\n-----------------\n");
     printf("La mayor complejidad la posee la funcion leer_entero() -> O(n^2)\n");
-    printf("La complejidad de la funcion pedida en el ejercicio, repetidos_con_ordinal(), tiene complejidad de O(m * n)\n");
-    printf("\n========= FIN DEL PROGRAMA =========\n");
+    printf("La complejidad de la funcion pedida en el ejercicio (repetidos_con_ordinal()) tiene complejidad de O(m * n)\n");
     return 0;
 }
