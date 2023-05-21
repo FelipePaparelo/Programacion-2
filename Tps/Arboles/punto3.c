5e695350b76c9c514d9c142171f1db0a8081bb31
Nahuel Ledesma <nahuledes17@gmail.com>
2023-05-21 15:00:37 -0300

    Update punto 3

--- Tps/Arboles/punto3.c
+++ Tps/Arboles/punto3.c
@@ -15,6 +15,7 @@ Lista hijos(ArbolBinario a, int clave);
 TipoElemento hermano(ArbolBinario a, int clave, TipoElemento padre);
 int altura(ArbolBinario a, int clave);
 int altura_subarbol(ArbolBinario a, int clave);
+Lista nivel_nodos(ArbolBinario a, int clave, int altura);
 
 void pre_orden(NodoArbol n);
 void in_orden(NodoArbol N);
@@ -22,19 +23,38 @@ void post_orden(NodoArbol N);
 
 bool verificar_entrada(int *valor);
 bool verificar_entrada_int(int *valor);
+bool verificar_nodo(ArbolBinario a, int clave);
 void cargar_arbol(ArbolBinario a);
 void cargar_subArbol(ArbolBinario a, NodoArbol n, int hijo);
 
 
 int main()
 {
+    // Árbol para realizar el ejercicio
     ArbolBinario a = a_crear();
+    // Nodo que va a ser seleccionado para hacer el ejercicio
     int nodo_clave;
+    // TipoElemento que va a contener el nombre del nodo padre 
+    // del nodo seleccionado (Ejercicio 3A)
     TipoElemento nodo_padre = te_crear(0);
+    // TipoElemento que va a contener el nombre del nodo hermano 
+    // del nodo seleccionado (Ejercicio 3B)
     TipoElemento nodo_hermano = te_crear(0);
+    // Lista que va a contener el/los hijos del nodo 
+    // seleccionado (Ejercicio 3C)
     Lista lista_hijos = l_crear();
+    // Entero que va a contener la altura 
+    // del nodo seleccionado (Ejercicio 3D)
     int altura_nodo;
+    // Entero que va a contener la altura del subarbol 
+    // del nodo seleccionado (Ejercicio 3E)
     int altura_rama;
+    // Lista que va a contener los nodos a la misma altura 
+    // del nodo seleccionado (Ejercicio 3F)
+    Lista lista_hermanos = l_crear();
+
+    bool flag = true;
+
 
     if (a_es_vacio(a)) {
         printf("El arbol se creo y esta vacio!\n");
@@ -42,7 +62,12 @@ int main()
 
     cargar_arbol(a);
 
-    // LLamo al mostrar arbol
+    if(a_es_vacio(a)){ 
+        printf("El árbol está vacío, fin del programa."); 
+        return 0;
+    }
+
+    // Se muestra el árbol en las diferentes formas de recorrido
     printf("-----------------------------------------------------\n");
     printf("Pre-Orden Desde la raiz\n");
     pre_orden(a_raiz(a));
@@ -56,15 +81,23 @@ int main()
     post_orden(a_raiz(a));
     printf("\n");
 
-    printf("Ingresar la clave del nodo con el que se va a realizar el ejercicio : ");
-    if(!verificar_entrada_int(&nodo_clave)){
-        printf("El dato de la clave no es un Entero");
-        return 0;
-    }else if(nodo_clave < -9999999 || nodo_clave > 9999999){
-        printf("El valor debe estar en el rango de -9999999 y 9999999");
-        return 0;
+    while (flag)
+    {
+        printf("Ingresar la clave del nodo con el que se va a realizar el ejercicio : ");
+        if(!verificar_entrada_int(&nodo_clave)){
+            printf("El dato de la clave no es un Entero");
+            return 0;
+        }else if(nodo_clave < -9999999 || nodo_clave > 9999999){
+            printf("El valor debe estar en el rango de -9999999 y 9999999\n");
+            //return 0;
+        }else if(verificar_nodo(a, nodo_clave) != true){ printf("El nodo seleccionado no está en el árbol. Seleccione otro válido\n"); }
+        else{
+            printf("Nodo seleccionado correcto!\n");
+            flag = false;
+        }
     }
-
+    
+    
     //-----------------------------------------------------------------
     // Obtener cual es la clave del nodo padre
     //-----------------------------------------------------------------
@@ -77,16 +110,20 @@ int main()
     // Obtener una lista con los hijos
     //-----------------------------------------------------------------
     lista_hijos = hijos(a, nodo_clave);
-    printf("Los hijos del nodo %d es :");
-    l_mostrarLista(lista_hijos);
+    if(l_es_vacia(lista_hijos) != true){ 
+        printf("Los hijos del nodo %d es :");
+        l_mostrarLista(lista_hijos);
+    }
+    else{ printf("El nodo %d NO tiene hijos.\n", nodo_clave); } 
 
 
     //-----------------------------------------------------------------
     // Obtener el Hermano del nodo elegido
     //-----------------------------------------------------------------
     nodo_hermano = hermano(a, nodo_clave, nodo_padre);
-    printf("El nodo hermano de %d es : %d\n", nodo_clave, nodo_hermano->clave);
-    
+    if(nodo_hermano == NULL){ printf("El nodo %d NO tiene hermanos\n", nodo_clave); }
+    else{ printf("El nodo hermano de %d es : %d\n", nodo_clave, nodo_hermano->clave); }
+     
 
     //-----------------------------------------------------------------
     // Obtener la altura en la que se encuentra la clave elegida
@@ -99,9 +136,20 @@ int main()
     //-----------------------------------------------------------------
     // Obtener la altura del subarbol
     //-----------------------------------------------------------------
-    altura_rama = altura_subarbol(a, nodo_clave);
+    altura_rama = altura_subarbol(a, nodo_clave) - 1;
     if(altura_rama < 0){ printf("La clave a buscar no existe en el arbol\n"); }
     else{ printf("La altura del SubArbol %d es : %d\n", nodo_clave, altura_rama); }
+
+
+    //-----------------------------------------------------------------
+    // Obtener todos los nodos de la altura de la clave
+    //-----------------------------------------------------------------
+    lista_hermanos = nivel_nodos(a, nodo_clave, altura_nodo);
+    if(l_es_vacia(lista_hermanos)){ printf("No hay nodos en el mismo nivel del nodo %d\n", nodo_clave); }
+    else{ 
+        printf("Los nodos que se encuentran al mismo nivel que %d son : ", nodo_clave); 
+        l_mostrarLista(lista_hermanos);
+    }
 }
 
 //-------------------------------------
@@ -195,24 +243,73 @@ bool verificar_entrada_int(int *valor)
 
 
 //---------------------------------------------------------
+// Verifica que el nodo ingresado esté en el árbol
+//---------------------------------------------------------
+void verificar_nodo_interno(NodoArbol q, int clave, bool *resultado){
+    TipoElemento x;
+
+    if(q == NULL) { }
+    else {
+        x = n_recuperar(q);
+
+        if(x->clave == clave){ *resultado = true; }
+
+        // Se hace el recorrido del arbol
+        verificar_nodo_interno(n_hijoizquierdo(q), clave, resultado);
+        verificar_nodo_interno(n_hijoderecho(q), clave, resultado);
+    }
+}
+
+bool verificar_nodo(ArbolBinario a, int clave){
+    bool resultado = false;
+    verificar_nodo_interno(a_raiz(a), clave, &resultado);
+    return resultado;
+}
+
+
+//---------------------------------------------------------
 // Cargar Arbol
 //---------------------------------------------------------
+void repetido_interno(NodoArbol q, int clave, bool *resultado){
+    TipoElemento x;
+    
+    if(q == NULL){}
+    else{
+        x = n_recuperar(q);
+        if(x->clave == clave){
+            *resultado = true;
+        }
+
+        repetido_interno(n_hijoizquierdo(q), clave, resultado);
+        repetido_interno(n_hijoderecho(q), clave, resultado);
+    }
+}
+
 void cargar_subArbol(ArbolBinario a, NodoArbol n, int hijo){
     TipoElemento x;
     NodoArbol n1;
     int valor_a_ingresar;
     bool entero;
+    bool repetido = false;
+
     if(!a_es_lleno(a)){
         entero = verificar_entrada(&valor_a_ingresar);
         if(entero){
-            x = te_crear(valor_a_ingresar);
+            if(a_es_vacio(a) != true) { repetido_interno(a_raiz(a), valor_a_ingresar, &repetido); }
 
-            if(hijo == -1){ n1 = a_conectar_hi(a, n, x); }
-            else if(hijo == 1){ n1 = a_conectar_hd(a, n, x); }
-            else n1 = a_establecer_raiz(a, x);
+            if(repetido != true){
+                x = te_crear(valor_a_ingresar);
 
-            cargar_subArbol(a, n1, -1);
-            cargar_subArbol(a, n1, 1);
+                if(hijo == -1){ n1 = a_conectar_hi(a, n, x); }
+                else if(hijo == 1){ n1 = a_conectar_hd(a, n, x); }
+                else n1 = a_establecer_raiz(a, x);
+
+                cargar_subArbol(a, n1, -1);
+                cargar_subArbol(a, n1, 1);
+            }else{
+                printf("El valor %d ya se encuentra en el árbol, ingrese otro\n", valor_a_ingresar);
+                cargar_subArbol(a, n, hijo);
+            }
         }
     }
 }
@@ -323,8 +420,11 @@ void hermano_interno(NodoArbol q, int clave, TipoElemento padre, TipoElemento* r
 
 TipoElemento hermano(ArbolBinario a, int clave, TipoElemento padre){
     TipoElemento resultado = te_crear(0);
-    hermano_interno(a_raiz(a), clave, padre, &resultado);
-    return resultado;
+    if(padre == NULL){ return NULL; }
+    else{ 
+        hermano_interno(a_raiz(a), clave, padre, &resultado); 
+        return resultado;
+    }
 }
 
 
@@ -355,7 +455,7 @@ int altura(ArbolBinario a, int clave){
 
 
 //---------------------------------------------------------
-// Altura de SubArbol. Punto 3E (Incompleto)
+// Altura de SubArbol. Punto 3E 
 //---------------------------------------------------------
 void altura_total_interno(NodoArbol q, int *altura, int nivel){
     if (q == NULL) {
@@ -391,4 +491,31 @@ int altura_subarbol(ArbolBinario a, int clave){
     nodo_a_buscar(a_raiz(a), clave, &nodo_actual);
     altura_total_interno(nodo_actual, &resultado, 0);
     return resultado;
+}
+
+
+//---------------------------------------------------------
+// Nodos del mismo nivel. Punto 3F
+//---------------------------------------------------------
+void nivel_nodos_interno(NodoArbol q, int altura, int nivel, Lista l){
+    TipoElemento x;
+
+    if(q == NULL) {}
+    else {
+        x = n_recuperar(q);
+        if(altura == nivel){
+            l_agregar(l, x);
+        }
+
+        nivel_nodos_interno(n_hijoizquierdo(q), altura, nivel+1, l);
+        nivel_nodos_interno(n_hijoderecho(q), altura, nivel+1, l);
+    }
+}
+
+Lista nivel_nodos(ArbolBinario a, int clave, int altura){
+    Lista lista_resultado = l_crear();
+    nivel_nodos_interno(a_raiz(a), altura, 0, lista_resultado);
+    l_borrar(lista_resultado, clave);
+
+    return lista_resultado;
 }
