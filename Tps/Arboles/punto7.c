069bcf7c55268986a06b27255c34ae69ff7491ed
Lucas Ortiz <lucas98ortizcorrea@gmail.com>
2023-05-22 17:32:37 -0300

    Punto7Modificado(noterminado)

--- Tps/Arboles/punto7.c
+++ Tps/Arboles/punto7.c
@@ -2,14 +2,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
 #include "arbol-binario.h"
 #include "tipo_elemento.h"
 #include "nodo.h"
 #include "Nodo_funciones.c"
 #include "I_arboles_binarios.c"
 
-// 7. Determinar si dos árboles binarios son equivalentes
+bool sonEquivalentes(ArbolBinario A, ArbolBinario B,NodoArbol NAA,NodoArbol NAB,bool equivalente){
+    if((!a_es_rama_nula(NAA) && !a_es_rama_nula(NAB))){
+        TipoElemento elemento1 = n_recuperar(NAA);
+        TipoElemento elemento2 = n_recuperar(NAB);
+        if((elemento1->clave) != (elemento2->clave)){
+            equivalente = false;
+        }
+        if(equivalente){
+            equivalente = sonEquivalentes(A,B,n_hijoizquierdo(NAA),n_hijoizquierdo(NAB),equivalente);
+            equivalente = sonEquivalentes(A,B,n_hijoderecho(NAA),n_hijoderecho(NAB),equivalente);
+        }
+
+    }
+    if((a_es_rama_nula(NAA) && !a_es_rama_nula(NAB)) || (a_es_rama_nula(NAB) && !a_es_rama_nula(NAA))){ //verifica que las estructuras sean iguales
+            equivalente =  false;
+    }
+    return equivalente;
+
+};
+
+int main() {
+    ArbolBinario arbolA = a_crear();
+    printf("[ARBOL A - INGRESO]\n");
+    cargar_arbol(arbolA);
+    ArbolBinario arbolB = a_crear();
+    printf("[ARBOL B - INGRESO]\n");
+    cargar_arbol(arbolB);
+    NodoArbol raizA = a_raiz(arbolA);
+    NodoArbol raizB = a_raiz(arbolB);
+    if (sonEquivalentes(arbolA, arbolB,raizA, raizB,true)){
+        printf("Los arboles ingresados son equivalentes\n");
+    } else {
+        printf("Los arboles ingresados NO son equivalentes\n");
+    }
+    return 0;
+}
 
 bool verificar_entrada(int *valor){
     char entrada[10];
@@ -29,7 +63,7 @@ bool verificar_entrada(int *valor){
         for (i; entrada[i] != '\0'; i++) {
             if (!isdigit(entrada[i])){
                 printf("El valor ingresado no es un entero, intente de nuevo\n");
-                verificar_entrada(valor);    
+                verificar_entrada(valor);
                 break;
             }else if((entrada[i]>='0')&&(entrada[i]<='9')){
                 *valor = *valor * 10 + (entrada[i] - 48);
@@ -63,37 +97,4 @@ void cargar_subArbol(ArbolBinario a, NodoArbol n, int hijo){
 
 void cargar_arbol(ArbolBinario a){
     cargar_subArbol(a, NULL, 0);
-}
-
-bool son_equivalentes(ArbolBinario arbolA, ArbolBinario arbolB, bool equivalentes, NodoArbol nodoActualA, NodoArbol nodoActualB) {
-    if (equivalentes && !a_es_rama_nula(nodoActualA) && !a_es_rama_nula(nodoActualB)) {
-        TipoElemento elementoA = n_recuperar(nodoActualA);
-        TipoElemento elementoB = n_recuperar(nodoActualB);
-        if (elementoA->clave != elementoB->clave) {
-            equivalentes = false;
-        }
-        son_equivalentes(arbolA, arbolB, equivalentes, n_hijoizquierdo(nodoActualA), n_hijoizquierdo(nodoActualB));
-        son_equivalentes(arbolA, arbolB, equivalentes, n_hijoderecho(nodoActualA), n_hijoderecho(nodoActualB));
-    } else {
-        return equivalentes;
-    }
-}
-
-int main() {
-    ArbolBinario arbolA = a_crear();
-    printf("[ARBOL A - INGRESO]\n");
-    cargar_arbol(arbolA);
-    ArbolBinario arbolB = a_crear();
-    printf("[ARBOL B - INGRESO]\n");
-    cargar_arbol(arbolB);
-    bool equivalentes = true;
-    NodoArbol raizA = a_raiz(arbolA);
-    NodoArbol raizB = a_raiz(arbolB);
-    bool sonEquivalentes = son_equivalentes(arbolA, arbolB, equivalentes, raizA, raizB);
-    if (sonEquivalentes) {
-        printf("Los arboles ingresados son equivalentes\n");
-    } else {
-        printf("Los arboles ingresados NO son equivalentes\n");
-    }
-    return 0;
 }
