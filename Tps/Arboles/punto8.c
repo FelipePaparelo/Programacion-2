fd2355eb5c6c0e9dd3d10761d118c3691181cd63
Lucas Ortiz <lucas98ortizcorrea@gmail.com>
2023-05-26 20:30:11 -0300

    Punto 8
    Falta el item c donde se calculan los nodos nternos. Ademas revisar la funcion de carga y en general la entrada de datos para cada funcion

---
+++ Tps/Arboles/punto8.c
@@ -0,0 +1,138 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include "arbol-binario.h"
+#include "tipo_elemento.h"
+#include "nodo.h"
+#include "Nodo_funciones.c"
+#include "I_arboles_binarios.c"
+
+bool verificar_entrada(int *valor){
+    char entrada[10];
+    bool resultado = true;
+    bool negativo = false;
+    int i = 0;
+    *valor=0;
+    printf("Ingrese un numero o '.' para nulo: ");
+    scanf("%s", entrada);
+    if (entrada[0] == '.'){
+        resultado = false;
+    }else{
+        if(entrada[0] == '-'){
+            negativo = true;
+            i++;
+        }
+        for (i; entrada[i] != '\0'; i++) {
+            if (!isdigit(entrada[i])){
+                printf("El valor ingresado no es un entero, intente de nuevo\n");
+                verificar_entrada(valor);
+                break;
+            } else if((entrada[i]>='0')&&(entrada[i]<='9')){
+                *valor = *valor * 10 + (entrada[i] - 48);
+            }
+        }
+        if(negativo){ *valor = *valor * (-1); }
+    }
+
+    return resultado;
+}
+
+void cargar_subArbol(ArbolBinario a, NodoArbol n, int hijo){
+    TipoElemento x;
+    NodoArbol n1;
+    int valor_a_ingresar;
+    bool entero;
+    if(!a_es_lleno(a)){
+        entero = verificar_entrada(&valor_a_ingresar);
+        if(entero){
+            x = te_crear(valor_a_ingresar);
+            if(hijo == -1){ n1 = a_conectar_hi(a, n, x); }
+            else if(hijo == 1){ n1 = a_conectar_hd(a, n, x); }
+            else n1 = a_establecer_raiz(a, x);
+            cargar_subArbol(a, n1, -1);
+            cargar_subArbol(a, n1, 1);
+        }
+    }
+}
+
+void cargar_arbol(ArbolBinario a) {
+    cargar_subArbol(a, NULL, 0);
+}
+
+bool esHoja(NodoArbol n){
+    return (n_hijoizquierdo(n) == NULL) && (n_hijoderecho(n) == NULL);
+}
+
+int alturaArbolNArio(NodoArbol nodo, int contador, int altura) {
+    if(!(nodo == NULL)) {
+        if(contador > altura) {
+            altura = contador;
+        }
+        altura = alturaArbolNArio(n_hijoizquierdo(nodo), contador + 1, altura);
+        altura = alturaArbolNArio(n_hijoderecho(nodo), contador, altura);
+    }
+    return altura;
+}
+
+int determinarNivelNodo(int claveNodoABuscar,NodoArbol nodoArbol,int contador, int nivelNodo){
+    if(nivelNodo == 0 && nodoArbol != NULL){
+        TipoElemento elemento1 = n_recuperar(nodoArbol);
+        if(elemento1->clave == claveNodoABuscar){
+            nivelNodo = contador;
+        }
+        nivelNodo = determinarNivelNodo(claveNodoABuscar,n_hijoizquierdo(nodoArbol),contador+1,nivelNodo);
+        nivelNodo = determinarNivelNodo(claveNodoABuscar,n_hijoderecho(nodoArbol),contador,nivelNodo);
+    }
+    return nivelNodo;
+};
+
+//ESTA ES MIA NO ES DEL EJERCICIO
+int nivelPrimerHoja(NodoArbol n,int nivel,int cont){
+    if((n != NULL) && (nivel == 0)){
+        if(esHoja(n)){
+            nivel = cont;
+        }
+        nivel = nivelPrimerHoja(n_hijoizquierdo(n),nivel,cont+1);
+    }
+    return nivel;
+}
+
+bool hojasEnMismaAltura(NodoArbol n,int nivel,int contador,bool resultado){
+    if((n!=NULL) && (resultado)){
+        if(esHoja(n)){
+            if(nivel != contador){
+                resultado = false;
+            }
+        }
+        resultado = hojasEnMismaAltura(n_hijoizquierdo(n),nivel,contador+1,resultado);
+        resultado = hojasEnMismaAltura(n_hijoderecho(n),nivel,contador,resultado);
+    }
+    return resultado;
+}
+
+int main(){
+    ArbolBinario arbolTransformado = a_crear();
+    printf("[ARBOL TRANSFORMADO - INGRESO]\n");
+    cargar_arbol(arbolTransformado);
+    // a. sacar la altura
+    /*if (!a_es_vacio(arbolTransformado)) {
+        int altura = alturaArbolNArio(a_raiz(arbolTransformado),1,1);
+        printf("La altura del arbol es de %d.\n", altura);
+    } else {
+        printf("El arbol no tiene altura ya que esta vacio.\n");
+    }*/
+
+    /*int resultado = determinarNivelNodo(7,a_raiz(arbolTransformado),1,0);
+    if( resultado != 0){
+        printf("Nivel %d: ",resultado);
+    }else{printf("No se encontro");}
+    return 0;*/
+
+    //bool mismoNivel =true;
+    //bool resultado = hojasEnMismaAltura(a_raiz(arbolTransformado),nivelPrimerHoja(a_raiz(arbolTransformado),0,1),1,mismoNivel);
+    //if(resultado){
+    //    printf("mismo nivel");
+    //}else{printf("distinto nivel");}
+}
