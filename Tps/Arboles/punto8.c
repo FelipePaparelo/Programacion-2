398bf95ddf69addb16434ccea42bec41be4d845e
Dante Terranova <93538499+BinaryJuan@users.noreply.github.com>
2023-05-30 22:15:55 -0300

    Update punto8.c

--- Tps/Arboles/punto8.c
+++ Tps/Arboles/punto8.c
@@ -8,8 +8,8 @@
 #include "nodo.h"
 #include "Nodo_funciones.c"
 #include "I_arboles_binarios.c"
-#include "listas_de_arreglos.c"
-//#include "listas_cursores.c"
+//#include "listas_de_arreglos.c"
+#include "listas_cursores.c"
 //#include "listas_punteros.c"
 
 void strtrim(char *cadena) {
@@ -48,57 +48,141 @@ int leer_entero() {
 }
 
 
-bool verificar_entrada(int *valor){
-    char entrada[10];
-    bool resultado = true;
-    bool negativo = false;
-    int i = 0;
-    *valor=0;
-    printf("Ingrese un numero o '.' para nulo: ");
-    scanf("%s", entrada);
-    if (entrada[0] == '.'){
-        resultado = false;
-    }else{
-        if(entrada[0] == '-'){
-            negativo = true;
-            i++;
+// bool verificar_entrada(int *valor){
+//     char entrada[10];
+//     bool resultado = true;
+//     bool negativo = false;
+//     int i = 0;
+//     *valor=0;
+//     printf("Ingrese un numero o '.' para nulo: ");
+//     scanf("%s", entrada);
+//     if (entrada[0] == '.'){
+//         resultado = false;
+//     }else{
+//         if(entrada[0] == '-'){
+//             negativo = true;
+//             i++;
+//         }
+//         for (i; entrada[i] != '\0'; i++) {
+//             if (!isdigit(entrada[i])){
+//                 printf("El valor ingresado no es un entero, intente de nuevo\n");
+//                 verificar_entrada(valor);
+//                 break;
+//             } else if((entrada[i]>='0')&&(entrada[i]<='9')){
+//                 *valor = *valor * 10 + (entrada[i] - 48);
+//             }
+//         }
+//         if(negativo){ *valor = *valor * (-1); }
+//     }
+//     return resultado;
+// }
+
+// void cargar_subArbol(ArbolBinario a, NodoArbol n, int hijo){
+//     TipoElemento x;
+//     NodoArbol n1;
+//     int valor_a_ingresar;
+//     bool entero;
+//     if(!a_es_lleno(a)){
+//         entero = verificar_entrada(&valor_a_ingresar);
+//         if(entero){
+//             x = te_crear(valor_a_ingresar);
+//             if(hijo == -1){ n1 = a_conectar_hi(a, n, x); }
+//             else if(hijo == 1){ n1 = a_conectar_hd(a, n, x); }
+//             else n1 = a_establecer_raiz(a, x);
+//             cargar_subArbol(a, n1, -1);
+//             cargar_subArbol(a, n1, 1);
+//         }
+//     }
+// }
+
+// void cargar_arbol(ArbolBinario a) {
+//     cargar_subArbol(a, NULL, 0);
+//     int c;
+//     while ((c = getchar()) != '\n' && c != EOF) {}
+// }
+
+bool validar_numeros(char *m)
+{
+    int aux = 0;
+    int bandera = 1;
+    bool numero;
+    fgets(m, 100, stdin);
+    int largo = strlen(m) - 1;
+    while (bandera == 1)
+    {
+        if (strcmp(m, "\n") == 0)
+        {
+            printf("Has ingresado un salto de linea.\n");
+            aux = -1;
         }
-        for (i; entrada[i] != '\0'; i++) {
-            if (!isdigit(entrada[i])){
-                printf("El valor ingresado no es un entero, intente de nuevo\n");
-                verificar_entrada(valor);
-                break;
-            } else if((entrada[i]>='0')&&(entrada[i]<='9')){
-                *valor = *valor * 10 + (entrada[i] - 48);
+
+        for (int j = 0; j < largo; j++)
+        {
+
+            if ((isdigit(m[j]) != 0))
+            {
+                aux++;
             }
+
+            if ((j == 0) && (m[j] == '-')&& (isdigit(m[j+1]) != 0))
+            {
+                aux++;
+            }
+
         }
-        if(negativo){ *valor = *valor * (-1); }
+
+        if (aux == largo && aux<6)
+        {
+            bandera = 0;
+        }
+        else if(m[0] == '.' && m[1]== '\n'){
+            bandera = 0;
+        }
+        else
+        {
+            aux = 0;
+            printf("El dato que Ingresa no es un numero o es mayor a 99.999 o menor a -9999, intente otra vez: ");
+            fgets(m, 100, stdin);
+            fflush(stdin);
+            largo = strlen(m) - 1;
+        }
     }
-    return resultado;
+    if (m[0] == '.'){
+        numero=false;
+    }
+    else{
+        numero=true;
+    }
+    return numero;
 }
 
 void cargar_subArbol(ArbolBinario a, NodoArbol n, int hijo){
     TipoElemento x;
     NodoArbol n1;
-    int valor_a_ingresar;
-    bool entero;
+    char cadena[100];
+    bool numero;
+    int numero_i;
+
     if(!a_es_lleno(a)){
-        entero = verificar_entrada(&valor_a_ingresar);
-        if(entero){
-            x = te_crear(valor_a_ingresar);
+        printf("Ingrese un numero o '.' para nulo: ");
+        numero=validar_numeros(cadena);
+        if (numero){
+
+            numero_i=atoi(cadena);
+            x = te_crear(numero_i);
+
             if(hijo == -1){ n1 = a_conectar_hi(a, n, x); }
             else if(hijo == 1){ n1 = a_conectar_hd(a, n, x); }
             else n1 = a_establecer_raiz(a, x);
+
             cargar_subArbol(a, n1, -1);
             cargar_subArbol(a, n1, 1);
         }
     }
 }
 
-void cargar_arbol(ArbolBinario a) {
+void cargar_arbol(ArbolBinario a){
     cargar_subArbol(a, NULL, 0);
-    int c;
-    while ((c = getchar()) != '\n' && c != EOF) {}
 }
 
 bool esHojaNArio(NodoArbol n){
